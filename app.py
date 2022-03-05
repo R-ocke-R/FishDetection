a428978e4887cd9c8707dc375abd86d4d9607993
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-03-05 19:03:08 +0530

    created new files

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return render_template("index.html")
+    # return f'Hello, {escape(name)}!'
 
 if __name__=="__main__":
     app.run(debug=True)
